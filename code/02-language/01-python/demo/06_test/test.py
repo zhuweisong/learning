fb96b5fe18b02558f1b32f371396e6e9b1fa6f28
sevenzhu <sevenzhu@tencent.com>
2024-02-29 23:48:08 +0800

    [python demo]使用pytest

--- code/02-language/01-python/demo/06_test/test.py
+++ code/02-language/01-python/demo/06_test/test.py
@@ -1,18 +1,18 @@
 import ecar
-
-import unittest
+import pytest
 
 # This is a test case
-# It is a class that inherits from unittest.TestCase
 # 用于测试方法的类
-# 继承自unittest.TestCase
-class TestEcar(unittest.TestCase):
-    def test_ecar(self):
-        self.assertEqual(ecar.get_name("a", "b"), "a_b")
+def test_ecar():
+    assert ecar.get_name("a", "b") == "a_b"
 
-    def test_show_battry(self):
-        my_ecar = ecar.ECar("make", "model", "year", 100)
-        self.assertEqual(my_ecar.show_battry(), 1000)
+def test_show_battry():
+    my_ecar = ecar.ECar("make", "model", "year", 100)
+    assert my_ecar.show_battry() == 100
 
-if __name__ == '__main__':
-    unittest.main()
+# Run the test case
+# 运行测试用例, 通过命令行执行
+# pytest -s test.py
+
+# 或者  通过代码执行
+#pytest.main(["-s", "test.py"])
